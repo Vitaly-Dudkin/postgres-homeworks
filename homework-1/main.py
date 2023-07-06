f1be9c9d3f812ef4a0c3cc19e2b8568320c11110
vitaly <dudkin231@mail.ru>
2023-07-06 17:30:54 +0300

    added cahnges

--- homework-1/main.py
+++ homework-1/main.py
@@ -2,8 +2,6 @@
 import psycopg2
 import csv
 
-PATH = './north_data/customers_data.csv'
-
 
 def add_data_to_customers(path='./north_data/customers_data.csv'):
     with open(path, 'r') as file:
@@ -16,7 +14,6 @@ def add_data_to_customers(path='./north_data/customers_data.csv'):
                                 user='postgres',
                                 password='qwerty')
         cur = conn.cursor()
-
         cur.executemany("INSERT INTO customers VALUES(%s,%s,%s)", content[1:])
         conn.commit()
         conn.close()
@@ -34,6 +31,30 @@ def add_data_to_employees(path='./north_data/employees_data.csv'):
                                 password='qwerty')
         cur = conn.cursor()
 
-        cur.executemany("INSERT INTO employees VALUES(%s,%s,%s,%s,%s)", content[1:])
+        cur.executemany(f"INSERT INTO employees VALUES(%s,%s,%s,%s,%s,%s)", content[1:])
         conn.commit()
         conn.close()
+
+
+# add_data_to_customers()
+# add_data_to_employees()
+
+def add_data_to_orders(path='./north_data/orders_data.csv'):
+    with open(path, 'r') as file:
+        csv_content = csv.reader(file, delimiter=',')
+        content = []
+        for i in csv_content:
+            content.append(i)
+        conn = psycopg2.connect(host='127.0.0.1',
+                                database='north',
+                                user='postgres',
+                                password='qwerty')
+        cur = conn.cursor()
+        print(content[1:])
+        cur.executemany(f"INSERT INTO orders VALUES(%s,%s,%s,%s,%s)", content[1:])
+
+        conn.commit()
+        conn.close()
+
+
+add_data_to_orders()
